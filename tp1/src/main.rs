30adb5853db78706cc00bd4d94107e444715682a
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2025-03-16 20:16:49 -0300

    Receiving the message in the route

--- tp1/src/main.rs
+++ tp1/src/main.rs
@@ -1,7 +1,14 @@
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_request(stream: TcpStream) {
-    println!("Connection succeeded")
+fn handle_request(mut stream: TcpStream) {
+    let buffer: &mut [u8] = &mut [0;100];
+    stream.read(buffer).unwrap();
+    // The http message
+    let message = String::from_utf8(Vec::from(buffer)).unwrap();
+    let route = message.split(" ").take(2).collect::<Vec<_>>()[1];
+    println!("Message: {:?}", message);
+    println!("Route: {:?}", route);
 }
 
 fn main() -> std::io::Result<()> {
@@ -10,7 +17,7 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_request(stream);
+                handle_request(stream)
             }
             Err(_) => { println!("Connection failed") }
         }
