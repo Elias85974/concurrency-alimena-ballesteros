72153ac7ebaf10983ed0c8dcc758b15a0a483be5
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2025-03-18 11:31:56 -0300

    First implementation working for pi calculation and response

--- tp1/src/main.rs
+++ tp1/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_request(mut stream: TcpStream) {
@@ -19,17 +19,79 @@ fn handle_request(mut stream: TcpStream) {
 
     //Get the first line of the message
     let first_line = lines[0];
+    let parts = first_line.split_whitespace().collect::<Vec<_>>();
+    //Verify that it is an HTTP call
+    if !parts[2].starts_with("HTTP") {
+        return;
+    }
 
-    // let buffer: &mut [u8] = &mut [0;100];
-    // stream.read(buffer).unwrap();
-    // // The http message
-    // let message = String::fromutf8_lossy(Vec::from(buffer)).unwrap();
-    // let route = message.split(" ").take(2).collect::<Vec<>>()[1];
-    // let parameters = &route.split("/").collect::<Vec<_>>()[1..];
-    // println!("Message: {:?}", message);
-    // println!("Route: {:?}", route);
-    // println!("Parameters: {:?}", parameters)
+    let route = parts[1].split("/").skip(1).collect::<Vec<_>>();
+    println!("Route: {:?}", route);
+
+    if route[0].eq("pi") {
+        let response = handle_pi(route);
+        println!("Response: {}", response);
+        stream.write_all(response.as_bytes()).unwrap()
+    }
 }
+
+fn handle_pi(route: Vec<&str>) -> String {
+    if route.len() < 2 {
+        return format_response(400, "Bad Request: Missing parameter");
+    }
+
+    let param = route[1];
+
+    if let Some(num) = param.strip_prefix(":") {
+        if let Ok(i) = num.parse::<u64>() {
+            let result = calculate_pi(i);
+            return format_response(200, &format!(
+                "Valor de pi para el termino {}: {}, (Tiempo: {}s)",
+                i, result, "0"
+            ));
+        }
+    }
+
+    format_response(400, "Bad Request: Invalid parameter")
+}
+
+fn calculate_pi(i: u64) -> f64 {
+    let mut pi = 0.0;
+    // (-1)^n
+    let mut sign = 1.0;
+
+    for n in 0..i {
+        //n is u64 so I convert it to f64
+        let iteration = sign / ((2 * n + 1) as f64);
+        pi += iteration;
+        //alternates the sign
+        sign *= -1.0
+    }
+    pi * 4.0
+}
+
+fn format_response(status_code: u16, body: &str) -> String {
+    format!(
+        "HTTP/1.1 {} OK\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{}",
+        status_code,
+        body.len(),
+        body
+    )
+}
+
+/*
+fn deconstruct_route(route: &str) -> Routes {
+    let full_request = route.split(":").collect();
+    let route = full_request[0]; /lol/lal/sas/
+    let params = full_request[1]; pi/ 20
+}
+
+struct Routes {
+    route: &'_ str,
+    params: &'_[str]
+}
+
+ */
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3030")?;
