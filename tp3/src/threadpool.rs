13720f89923584332346455f229a46c6d35b4357
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2025-04-01 12:58:38 -0300

    Threadpool execution implementation

--- tp3/src/threadpool.rs
+++ tp3/src/threadpool.rs
@@ -22,4 +22,12 @@ impl ThreadPool {
 
         ThreadPool { workers, sender }
     }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
 }
