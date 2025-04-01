bad97feacb82c640becfc977797636947e02255f
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2025-04-01 12:56:11 -0300

    Worker and Threadpool "new()" implementation

--- tp3/src/threadpool.rs
+++ tp3/src/threadpool.rs
@@ -1,4 +1,4 @@
-use std::sync::{mpsc};
+use std::sync::{mpsc, Arc, Mutex};
 use crate::worker::Worker;
 
 pub struct ThreadPool {
@@ -6,4 +6,20 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
+pub type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+}
--- tp3/src/worker.rs
+++ tp3/src/worker.rs
@@ -1,5 +1,23 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+use crate::threadpool::Job;
+
 pub struct Worker {
     id: usize,
     thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub(crate) fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} recibió un trabajo; ejecutando.", id);
+            job();
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
 }
